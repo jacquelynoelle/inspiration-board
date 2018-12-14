4ae0d54213532143ecac93cf7e1ca3a2d19aec4c
jackie <jacquelyn.cheng@me.com>
2018-12-14 09:53:03 -0800

    cards showing static data with emoji on board

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,18 +8,32 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 class Board extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       cards: [],
     };
   }
 
+  componentDidMount() {
+    this.setState({
+      cards: CARD_DATA.cards,
+    });
+  }
+
+  generateCardList = () => {
+    const { cards } = this.state
+
+    return cards.map( (card, i) => {
+      return <Card key={ i } cardText={ card.text } cardEmoji={ card.emoji }/>
+    });
+  }
+
   render() {
     return (
       <div>
-        Board
+        { this.generateCardList() }
       </div>
     )
   }
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,17 +5,29 @@ import emoji from 'emoji-dictionary';
 import './Card.css';
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  generateEmoji = () => {
+    const emoji = require("emoji-dictionary");
+    const { cardEmoji } = this.props
+
+    return cardEmoji && emoji.getUnicode(cardEmoji);
+  }
+
   render() {
     return (
       <div className="card">
-        Card
+        { this.props.cardText }
+        { this.generateEmoji() }
       </div>
     )
   }
 }
 
 Card.propTypes = {
-
+  cardText: PropTypes.string
 };
 
 export default Card;
